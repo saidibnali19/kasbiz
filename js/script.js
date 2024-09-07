97aa29be7688b81ae6c3f42e675a309e337800ed
Said Ali <saidibnali19@gmail.com>
2024-09-07 14:49:32 +0300

    feat: added fade in animations to hero elements

--- js/script.js
+++ js/script.js
@@ -24,7 +24,7 @@ document.addEventListener("click", e => {
 const changingNav = document.querySelector("[data-primary-nav]");
 const hero = document.querySelector("[data-hero]")
 
-const fadeInOptions = {
+const fadeInNavOptions = {
     rootMargin: "-106px 0px 0px 0px",
     threshold: 0,
 };
@@ -37,6 +37,76 @@ const fadeInObserver = new IntersectionObserver((entries, fadeInObserver) => {
             changingNav.classList.remove("change")
         }
     });
-}, fadeInOptions);
+}, fadeInNavOptions);
 
 fadeInObserver.observe(hero);
+
+// Fade in animation for hero image
+const fadeInImage = document.querySelector("[data-fade-in-image]");
+
+const fadeInOptions = {
+    rootMargin: "0px",
+    threshold: 0,
+};
+
+const fadeInImageObserver = new IntersectionObserver((entries, fadeInImageObserver) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) return;
+        entry.target.classList.add("appear");
+        fadeInImageObserver.unobserve(entry.target);
+    });
+}, fadeInOptions);
+
+fadeInImageObserver.observe(fadeInImage);
+
+// Fade in animation for hero top text
+const fadeInTopText = document.querySelector("[data-fade-in-top-text]");
+
+const fadeInTopTextObserver = new IntersectionObserver((entries, fadeInTopTextObserver) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) return;
+        entry.target.classList.add("top-text-appear");
+        fadeInTopTextObserver.unobserve(entry.target);
+    });
+}, fadeInOptions);
+
+fadeInTopTextObserver.observe(fadeInTopText);
+
+// Fade in animation for hero title
+const fadeInTitle = document.querySelector("[data-fade-in-title]");
+
+const fadeInTitleObserver = new IntersectionObserver((entries, fadeInTitleObserver) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) return;
+        entry.target.classList.add("title-appear");
+        fadeInTitleObserver.unobserve(entry.target);
+    });
+}, fadeInOptions);
+
+fadeInTitleObserver.observe(fadeInTitle);
+
+// Fade in animation for hero text
+const fadeInText = document.querySelector("[data-fade-in-text]");
+
+const fadeInTextObserver = new IntersectionObserver((entries, fadeInTextObserver) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) return;
+        entry.target.classList.add("text-appear");
+        fadeInTextObserver.unobserve(entry.target);
+    });
+}, fadeInOptions);
+
+fadeInTextObserver.observe(fadeInText);
+
+// Fade in animation for hero cta
+const fadeInCta = document.querySelector("[data-fade-in-cta]");
+
+const fadeInCtaObserver = new IntersectionObserver((entries, fadeInCtaObserver) => {
+    entries.forEach((entry) => {
+        if (!entry.isIntersecting) return;
+        entry.target.classList.add("cta-appear");
+        fadeInCtaObserver.unobserve(entry.target);
+    });
+}, fadeInOptions);
+
+fadeInCtaObserver.observe(fadeInCta);
